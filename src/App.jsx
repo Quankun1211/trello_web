04ccc07272008c422be4c10d45ccc2b255bedd39
Quankun1211 <quan7ammm@gmail.com>
2024-01-15 20:12:56 +0700

    theming

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import './App.css'
 import Button from '@mui/material/Button'
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
-
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
+import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import  Typography  from '@mui/material/Typography'
 
 function App() {
 
   return (
     <>
-      
-    
+    <h1>hi</h1>
+      <Button variant='text'>Text</Button>
+      <Typography variant='body2' color='text.secondary'>abc</Typography>
     </>
   )
 }
